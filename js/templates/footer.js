1c77e1f54ecdd61587a3cf3b3b020a76621a97c8
Robert Peterson <petersrl30@gmail.com>
2019-04-12 00:26:20 -0400

    Having fun with js templating

---
+++ js/templates/footer.js
@@ -0,0 +1,6 @@
+document.write('\
+<script src="js/vendor/jquery-3.3.1.min.js"></script>\
+<script src="js/vendor/modernizr-3.6.0.min.js"></script>\
+<script src="css/bootstrap-4.2.1-dist/js/bootstrap.min.js"></script>\
+<script src="js/popper.min.js"></script>\
+');
---
+++ js/templates/header.js
@@ -0,0 +1,4 @@
+document.write('\
+<link href="css/bootstrap-4.2.1-dist/css/bootstrap.min.css" rel="stylesheet">\
+<link href="css/nav.css" rel="stylesheet">\
+');
